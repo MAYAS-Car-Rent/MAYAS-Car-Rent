a0a515157466b1468aa13e5212bbd890e1cf8b0f
AbdUlrahmanJaran <abdulrahmanjaran@gmail.com>
2022-05-14 22:46:56 +0300

    Adding Employee Controller

---
+++ MAYAS-Car-Rent/MAYAS-Car-Rent/Controllers/EmployeesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MAYAS_Car_Rent.Data;
+using MAYAS_Car_Rent.Models;
+using MAYAS_Car_Rent.Models.Interface;
+using MAYAS_Car_Rent.Models.DTOs;
+
+namespace MAYAS_Car_Rent.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase //Made by AbdUlrahman Jaran
+    {
+        private readonly IEmployee _employee;
+
+        public EmployeesController(IEmployee employee)
+        {
+            _employee = employee;
+        }
+
+        // GET: api/Employees/
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EmployeeDTO>>> GetEmployees()
+        {
+            var employees = await _employee.GetEmployees();
+            return Ok(employees);
+        }
+
+        // GET: api/Employees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EmployeeDTO>> GetEmployee(int id)
+        {
+            EmployeeDTO employee = await _employee.GetEmployee(id);
+            return Ok(employee);
+        }
+
+        // PUT: api/Employees/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            var updatedEmployee = await _employee.UpdateEmployee(id, employee);
+
+            return Ok(updatedEmployee);
+        }
+
+        // POST: api/Employees
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            return await _employee.CreateEmployee(employee);
+        }
+
+        // DELETE: api/Employees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            await _employee.DeleteEmployee(id);
+            return NoContent();
+        }
+
+    }
+}
